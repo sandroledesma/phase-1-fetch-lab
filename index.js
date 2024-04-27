ad7c0e350e2186fc878b4df7785a103abce2a373
Alessandro <sandroledesma@gmail.com>
2024-04-27 19:01:59 -0400

    add files

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((response) => response.json())
+  .then(renderBooks) 
+  .catch((error) => { console.log(error); });
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
